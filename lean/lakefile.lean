b9402e99db90a67626769c946ef1814ffd9a5405
Alexander K. Z. <grunichev.stanislav@icloud.com>
2026-02-07 01:51:41 +0300

    Create lakefile.lean

---
+++ lean/lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package «ogp_barrier» where
+  moreLeanArgs := #["-DautoImplicit=false"]
+
+@[default_target]
+lean_lib «OGPBarrier» where
+  roots := #[`OGPBarrier]
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
